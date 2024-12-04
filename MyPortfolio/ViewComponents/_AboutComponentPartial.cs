0400e82df5acd31c58570975d24bdc62bedfe754
Sbars1 <stem.bars99@gmail.com>
2024-12-05 01:21:53 +0300

    about halledildi

--- MyPortfolio/ViewComponents/_AboutComponentPartial.cs
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -5,16 +5,18 @@ namespace MyPortfolio.ViewComponents
 {
     public class _AboutComponentPartial  : ViewComponent
     {
-        MyPortfolioContext portfolioContext = new MyPortfolioContext();
+        MyPortfolioContext context = new MyPortfolioContext();
         public IViewComponentResult Invoke() {
 
 
-            ViewBag.aboutTitle = portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
-            ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
-            ViewBag.aboutDetail = portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault();
-         
+            //ViewBag.aboutTitle = portfolioContext.Abouts.Select(x => x.Title).FirstOrDefault();
+            //ViewBag.aboutSubDescription = portfolioContext.Abouts.Select(x => x.SubDescription).FirstOrDefault();
+            //ViewBag.aboutDetail = portfolioContext.Abouts.Select(x => x.Details).FirstOrDefault();
+            //ViewBag.aboutstatus = portfolioContext.Abouts.Select(x => x.Status==true).ToList();
 
-            return View();
+            var values = context.Abouts.Where(x=>x.Status==true).FirstOrDefault();
+            return View(values);
+
         }
 
   
