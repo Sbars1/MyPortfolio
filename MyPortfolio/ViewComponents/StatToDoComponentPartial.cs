df20c46fa81a65216130ac5e2bd118843083bed3
Sbars1 <stem.bars99@gmail.com>
2024-12-04 23:59:44 +0300

    istatistik ekleme

---
+++ MyPortfolio/ViewComponents/StatToDoComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents
+{
+	public class StatToDoComponentPartial :ViewComponent
+	{
+		MyPortfolioContext context =new MyPortfolioContext();
+		public IViewComponentResult Invoke()
+		{
+			var toDoLists = context.ToDoLists.Where(x => x.Status == false).ToList(); // Yapilmamis gorevler
+
+			return View(toDoLists);
+		}
+	}
+}
